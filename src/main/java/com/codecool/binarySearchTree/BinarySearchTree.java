b0bc265e259c565e346fd7f926f925dfc48a951d
szymslow12 <szym.slow12@gmail.com>
2018-09-13 22:54:02 +0200

    add BinarySearchTree

---
+++ src/main/java/com/codecool/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,104 @@
+package com.codecool.binarySearchTree;
+
+public class BinarySearchTree {
+
+    private Node root;
+
+    public BinarySearchTree(Integer[] elements) {
+        root = buildNodes(elements);
+    }
+
+    public static BinarySearchTree buildTree(Integer[] intArray) {
+        return new BinarySearchTree(intArray);
+    }
+
+
+
+    private Node buildNodes(Integer[] elements) {
+        if (elements.length > 0) {
+            int middle = (int) Math.floor(elements.length / 2);
+        }
+        return null;
+    }
+
+
+    public void add(Integer value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        if (value <= root.getValue()) {
+            Node leftSiteOfRoot = root.leftNode();
+            if (leftSiteOfRoot == null)
+                root.setLeftNode(newNode);
+            addNodeToProperParent(leftSiteOfRoot, newNode);
+        } else {
+            Node rightSiteOfRoot = root.rightNode();
+            if (rightSiteOfRoot == null)
+                root.setRightNode(newNode);
+            addNodeToProperParent(rightSiteOfRoot, newNode);
+        }
+    }
+
+    public void addNodeToProperParent(Node parent, Node newNode) {
+        while (parent != null) {
+            System.out.print(newNode.getValue() + " - " + parent.getValue());
+            if (newNode.getValue() <= parent.getValue()) {
+                if (parent.leftNode() == null) {
+                    parent.setLeftNode(newNode);
+                    return;
+                } else {
+                    parent = parent.leftNode();
+                }
+            } else {
+                if (parent.rightNode() == null) {
+                    parent.setRightNode(newNode);
+                    return;
+                } else {
+                    parent = parent.rightNode();
+                }
+            }
+        }
+    }
+
+    public int minimalHeight() {
+        return minimalHeight(root);
+    }
+
+
+    public int minimalHeight(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.leftNode() == null && root.rightNode() == null) {
+            return 1;
+        }
+
+        if (root.leftNode() == null) {
+            return minimalHeight(root.rightNode()) + 1;
+        }
+
+        if (root.rightNode() == null) {
+            return minimalHeight(root.leftNode()) + 1;
+        }
+        return Math.min(minimalHeight(root.leftNode()),
+                minimalHeight(root.rightNode())) + 1;
+    }
+
+
+
+
+    public static int minimalHeight(Integer[] intArray, int minimalHeight) {
+        BinarySearchTree bsn = buildTree(intArray);
+        return bsn.minimalHeight();
+    }
+
+
+    public static void main(String[] args) {
+        Integer[] intArray = {5, 1, 3, 5, 0, 4 ,5 ,6 ,7 ,8};
+        System.out.println(minimalHeight(intArray, 13));
+    }
+}
