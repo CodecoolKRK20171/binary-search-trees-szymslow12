afccad2631a9570db0546285e50f4c56a2840b9f
szymslow12 <szym.slow12@gmail.com>
2018-09-14 16:06:18 +0200

    move main() from BinarySearchTree to Main

--- src/main/java/com/codecool/binarySearchTree/BinarySearchTree.java
+++ src/main/java/com/codecool/binarySearchTree/BinarySearchTree.java
@@ -145,23 +145,4 @@ public class BinarySearchTree {
         return Math.min(minimalHeight(root.leftNode()),
                 minimalHeight(root.rightNode())) + 1;
     }
-
-
-    public static void main(String[] args) {
-        Integer[] numberArray = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        BinarySearchTree binarySearchTree = buildTree(numberArray);
-
-        System.out.println("Minimal height -> Expected: 3, Actual: " + binarySearchTree.minimalHeight());
-        System.out.println("Case: find 5, Expected: true, Actual: " + binarySearchTree.searchElement(5));
-        System.out.println("Case: find 0, Expected: true, Actual: " + binarySearchTree.searchElement(0));
-        System.out.println("Case: find 9, Expected: true, Actual: " + binarySearchTree.searchElement(9));
-        System.out.println("Case: find -1, Expected: false, Actual: " + binarySearchTree.searchElement(-1));
-        System.out.println("Case: find 10, Expected: false, Actual: " + binarySearchTree.searchElement(10));
-
-        binarySearchTree.add(234);
-        binarySearchTree.remove(1);
-        binarySearchTree.remove(3);
-
-        System.out.println("Minimal height -> Expected: 2, Actual: " + binarySearchTree.minimalHeight());
-    }
 }
