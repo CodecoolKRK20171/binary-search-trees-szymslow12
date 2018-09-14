33255d2ee2a439cf7b899a70398c299b92d1ee1b
szymslow12 <szym.slow12@gmail.com>
2018-09-14 15:48:07 +0200

    implement searchElement

--- src/main/java/com/codecool/binarySearchTree/BinarySearchTree.java
+++ src/main/java/com/codecool/binarySearchTree/BinarySearchTree.java
@@ -82,6 +82,17 @@ public class BinarySearchTree {
 
 
     public boolean searchElement(Integer value) {
+        Node currentNode = root;
+
+        while (currentNode != null) {
+            if (currentNode.getValue() == value) {
+                return true;
+            } else if (value <= currentNode.getValue()) {
+                currentNode = currentNode.leftNode();
+            } else {
+                currentNode = currentNode.rightNode();
+            }
+        }
         return false;
     }
 
@@ -119,6 +130,13 @@ public class BinarySearchTree {
         }
         BinarySearchTree bst = buildTree(intArray);
         System.out.println(bst.minimalHeight());
-        System.out.println(new BinarySearchTree(new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}).minimalHeight());
+        Integer[] numberArray = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        BinarySearchTree binarySearchTree = buildTree(numberArray);
+        System.out.println(binarySearchTree.minimalHeight());
+        System.out.println("Case: find 5, Expected: true, Actual: " + binarySearchTree.searchElement(5));
+        System.out.println("Case: find 0, Expected: true, Actual: " + binarySearchTree.searchElement(0));
+        System.out.println("Case: find 9, Expected: true, Actual: " + binarySearchTree.searchElement(9));
+        System.out.println("Case: find -1, Expected: false, Actual: " + binarySearchTree.searchElement(-1));
+        System.out.println("Case: find 10, Expected: false, Actual: " + binarySearchTree.searchElement(10));
     }
 }
